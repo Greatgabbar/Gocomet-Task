0cad77649fbf958864a35d9913a6d53d4b7df982
greatgabbar <strivedi_be19@thapar.edu>
2021-12-11 15:07:30 +0530

    navbar don

--- src/Components/Header/Nav.js
+++ src/Components/Header/Nav.js
@@ -1,15 +1,17 @@
 import classes from "./Nav.module.css";
 import { Link } from "react-router-dom";
-import logo from "../../../public/assets/images/logo.png";
+import logo from "../../assets/images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faShoppingBag, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
   return (
     <div className={classes.Nav}>
-      <div className={classes.Right}>
-        <div className={classes.logo}>
-          <img src={logo} alt="Myntra logo" />
-        </div>
+      <div className={classes.Left}>
+        {/* <div className={classes.logo}></div> */}
         <div className={classes.Links}>
+          <img src={logo} alt="Myntra logo" />
           <Link to={"#"}>Men</Link>
           <Link to={"#"}>Women</Link>
           <Link to={"#"}>Kids</Link>
@@ -17,9 +19,30 @@ const Nav = () => {
           <Link to={"#"}>Beauty</Link>
         </div>
       </div>
-      <div>
-        <div className={classes.Search}></div>
-        <div className={classes.Icons}></div>
+      <div className={classes.Right}>
+        <div className={classes.Search}>
+          <div>
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
+          <input
+            type="search"
+            placeholder="Search for products, brands and more"
+          />
+        </div>
+        <div className={classes.Icons}>
+          <div>
+            <FontAwesomeIcon icon={faUser} />
+            <p>Profile</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faHeart} />
+            <p>Wishlist</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faShoppingBag} />
+            <p>Bag</p>
+          </div>
+        </div>
       </div>
     </div>
   );
